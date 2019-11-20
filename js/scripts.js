399e170b8e796899d97ff437aca00f603ffd4194
Carla Mendes <carlarsmendes@gmail.com>
2019-11-20 16:40:05 +0000

    almost finished repsonsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,45 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+let $menuIcon = document.getElementById("icon-navbar");
+
 function myFunction() {
   var $navBar = document.getElementById("myTopnav");
   var $crossIcon = document.getElementById("icon-navbar");
-  if ($navBar.className === "topnav") {
+
+  console.log($navBar);
+  if ($navBar.className === "floating-fixed-element topnav") {
     $navBar.className += " responsive";
     $crossIcon.className = "fa fa-times";
     $crossIcon.innerText = "";
-  } else {
-    $navBar.className = "topnav";
+    console.log("enter the if");
+    console.log("$navBar.className-->", $navBar.className);
+    //Target all the links
+    let links = $navBar.getElementsByTagName("a");
+    // links.forEach(elem => {
+    //   elem.addEventListener("click", event => {
+    //     $navBar.className = "floating-fixed-element topnav";
+    //     $crossIcon.className = "fa fa-bars";
+    //     $crossIcon.innerText = " MENU";
+    //   });
+    // });
+    for (let i = 0; i < links.length; i++) {
+      let elem = links[i];
+      elem.addEventListener("click", () => {
+        $navBar.className = "floating-fixed-element topnav";
+        $crossIcon.className = "fa fa-bars";
+        $crossIcon.innerText = " MENU";
+      });
+      //   // elem.onclick =
+    }
+  } else if (($navBar.className = "floating-fixed-element topnav")) {
+    $navBar.className = "floating-fixed-element topnav";
     $crossIcon.className = "fa fa-bars";
     $crossIcon.innerText = " MENU";
+    console.log("enter the else");
   }
 }
+
+$menuIcon.addEventListener("click", myFunction);
+
+var $navBar = document.getElementById("myTopnav");
+console.log($navBar.className);
