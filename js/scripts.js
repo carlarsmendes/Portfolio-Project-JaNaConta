357024312dd2dd5a5d6ee45ffbf55508fe367a14
Carla Mendes <carlarsmendes@gmail.com>
2019-11-25 00:17:35 +0000

    added jQuery for smooth behavior

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,32 @@ $menuIcon.addEventListener("click", myFunction);
 
 var $navBar = document.getElementById("myTopnav");
 console.log($navBar.className);
+
+// -------EXPERIMENTAL SMOOTH SCROLL FIX FOR SAFARI------
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(t) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var e = $(this.hash);
+      (e = e.length ? e : $("[name=" + this.hash.slice(1) + "]")),
+        e.length &&
+          (t.preventDefault(),
+          $("html, body").animate(
+            {
+              scrollTop: e.offset().top
+            },
+            600,
+            function() {
+              var t = $(e);
+              if ((t.focus(), t.is(":focus"))) return !1;
+              t.attr("tabindex", "-1"), t.focus();
+            }
+          ));
+    }
+  });
